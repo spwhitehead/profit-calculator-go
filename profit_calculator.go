a5ca22652dfff2ebcdd65498706fcf4db928f911
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-14 11:16:22 -0600

    Initial commit of files
    Inits go.mod and contains a basic layout for the profit calulator inputs

---
+++ profit_calculator.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+
+	var annualRevenue float64
+	var annualExpenses float64
+	var taxRate float64
+	var ebt float64
+	var profit float64
+	var ratio float64
+
+	fmt.Print("What is your annual revenue? ")
+	fmt.Scan(annualRevenue)
+
+	fmt.Print("What are your annual expenses (in $)? ")
+	fmt.Scan(annualExpenses)
+
+	fmt.Print("What is your tax rate? ")
+	fmt.Scan(taxRate)
+
+	// Calculations
+
+	
+
+
+
+}

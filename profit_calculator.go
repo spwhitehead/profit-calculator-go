dc5c1bc4024fbf7f6114ee210453f33cd394c812
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-15 11:42:52 -0600

    Implements functions for printing and calculations

--- profit_calculator.go
+++ profit_calculator.go
@@ -10,28 +10,55 @@ func main(){
 	var annualExpenses float64
 	var taxRate float64
 
-	fmt.Print("What is your annual revenue? ")
+	getValue("Annual Revenue: ")
 	fmt.Scan(&annualRevenue)
 
-	fmt.Print("What are your annual expenses (in $)? ")
+	getValue("Annual Expenses: ")
 	fmt.Scan(&annualExpenses)
 
-	fmt.Print("What is your tax rate? ")
+	getValue("Tax Rate: ")
 	fmt.Scan(&taxRate)
+
+	ebt, profit, ratio := calculations(annualRevenue, annualExpenses, taxRate)
+
+	fmt.Printf(`
+	Earnings Before Tax:	 %.2f
+	Profit: 		 %.2f
+	Ratio (ebt/profit): 	 %.2f`, ebt, profit, ratio)
+	// fmt.Print("What is your annual revenue? ")
+	// fmt.Scan(&annualRevenue)
+
+	// fmt.Print("What are your annual expenses (in $)? ")
+	// fmt.Scan(&annualExpenses)
+
+	// fmt.Print("What is your tax rate? ")
+	// fmt.Scan(&taxRate)
 
 	// Calculations
 
-	earningsBeforeTax := annualRevenue - annualExpenses
-	profit := earningsBeforeTax * (1-taxRate/100)
-	ratio := earningsBeforeTax / profit
+	// earningsBeforeTax := annualRevenue - annualExpenses
+	// profit := earningsBeforeTax * (1-taxRate/100)
+	// ratio := earningsBeforeTax / profit
 
-	fmt.Print("Earnings Before Tax: ")
-	fmt.Println(earningsBeforeTax)
+	// fmt.Print("Earnings Before Tax: ")
+	// fmt.Println(earningsBeforeTax)
 
-	fmt.Print("Profit: ")
-	fmt.Println(profit)
+	// fmt.Print("Profit: ")
+	// fmt.Println(profit)
 
-	fmt.Print("Ratio (EBT/Profit): ")
-	fmt.Println(ratio)
+	// fmt.Print("Ratio (EBT/Profit): ")
+	// fmt.Println(ratio)
+
+}
+
+func getValue(value string){
+	fmt.Print(value)
+}
+
+func calculations(annualRevenue, annualExpenses, taxRate float64) (ebt float64, profit float64, ratio float64){
+	ebt = annualRevenue - annualExpenses
+	profit = ebt * (1-taxRate/100)
+	ratio = ebt / profit
+	return ebt, profit, ratio
 
 }
